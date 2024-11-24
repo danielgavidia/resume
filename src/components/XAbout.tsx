fa1763e12aca72ae5b7681617d8b295c5244d76a
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-24 12:30:24 -0500

    Added basic about component

---
+++ src/components/XAbout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const XAbout = () => {
+  return (
+    <div>
+      <div>
+        <p>Daniel Gavidia</p>
+        <p>Software Development, Finance</p>
+        <p>Brooklyn, NY</p>
+        <div>
+          <div></div>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default XAbout;
