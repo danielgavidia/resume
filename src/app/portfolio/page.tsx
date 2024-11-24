22287fefb20f6e5d2360d542f7f0dbbb326556b9
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-24 12:16:36 -0500

    Added styling to portfolio item component

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import XPortfolioItem from "@/components/XPortfolioItem";
+import { portfolioData } from "@/lib/portfolioData";
 
 const Page = () => {
-  return <div></div>;
+  return (
+    <div className="p-4 flex flex-col space-y-2">
+      {portfolioData.map((item, key) => (
+        <XPortfolioItem key={key} portfolioItem={item} />
+      ))}
+    </div>
+  );
 };
 
 export default Page;
