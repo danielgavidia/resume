c3a1b39ab982ce4e608a934cdf0541f4bd42d05d
Daniel Gavidia <dgavidia1@gmail.com>
2025-01-09 12:58:54 -0500

    Changes

--- src/lib/portfolioData.ts
+++ src/lib/portfolioData.ts
@@ -1,22 +1,22 @@
 import { PortfolioItem } from "@/types/types";
 
 export const portfolioData: PortfolioItem[] = [
-  // {
-  //   id: "3",
-  //   title: "Bouncy Art",
-  //   description:
-  //     "An interactive web application that generates unique live-action art pieces. Users can customize parameters and watch their creations come to life with playful animations.",
-  //   githubLink: "https://github.com/danielgavidia/fractal-ai-art",
-  //   deploymentLink: "https://bouncy-art.com/",
-  // },
-  // {
-  //   id: "4",
-  //   title: "Hangman",
-  //   description:
-  //     "A modern take on the classic Hangman game built with React. Features multiple difficulty levels, word categories, and a sleek user interface with real-time score tracking.",
-  //   githubLink: "https://github.com/danielgavidia/fractal-hangman",
-  //   deploymentLink: "https://fractal-hangman.com/",
-  // },
+  {
+    id: "6",
+    title: "Bouncy Art",
+    description:
+      "An interactive web application that generates unique live-action art pieces. Users can customize parameters and watch their creations come to life with playful animations.",
+    githubLink: "https://github.com/danielgavidia/fractal-ai-art",
+    deploymentLink: "https://bouncy-art.com/",
+  },
+  {
+    id: "7",
+    title: "Hangman",
+    description:
+      "A modern take on the classic Hangman game built with React. Features multiple difficulty levels, word categories, and a sleek user interface with real-time score tracking.",
+    githubLink: "https://github.com/danielgavidia/fractal-hangman",
+    deploymentLink: "https://fractal-hangman.com/",
+  },
   {
     id: "1",
     title: "Gavidia Enterprise",
